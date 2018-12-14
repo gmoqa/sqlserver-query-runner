4a523573103c869d5b3ceedb7b4682c8ce730938
Julio Pena <jpena@option.cl>
2018-12-14 01:35:08 -0300

    fiexD

--- app.js
+++ app.js
@@ -3,27 +3,30 @@ const bodyParser = require('body-parser')
 const sql = require('mssql')
 const cors = require('cors')
 const app = express()
-
+const asyncHandler = require('express-async-handler')
+ 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
-const conn = 'mssql://sa:####plfrg@localhost/MPCatchSB'
+const config = {
+    user: 'sa',
+    passsword: '####plfrg',
+    server: 'localhost',
+    database: 'MPCatchSB'
+}
 
 app.get('/status', (req, res) => {
     res.send('Ok :)')
 })
 
-app.post('/sql', async (req, res) => {
-    try {
-        await sql.connect(conn)
-        const result = await sql.query`${req.body.query}`
-        console.log(result)
-        res.send(result)
-    } catch (err) {
-        console.log(err)
-    }
-})
+app.post('/sql', asyncHandler(async (req, res, next) => {
+    let pool = await sql.connect(config)
+    const result = await pool.request()
+        .query(`${req.body.query}`);
+    console.dir(result)
+    res.send(result)
+}))
 
 app.listen(3002, () => {
     console.log('Running')
